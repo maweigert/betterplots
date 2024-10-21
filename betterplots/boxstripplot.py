60837f1d9ac56a269642230d0e1ae7c0a5b5de9a
Martin Weigert <marweigert@gmail.com>
2024-10-21 17:18:33 +0200

    format

--- betterplots/boxstripplot.py
+++ betterplots/boxstripplot.py
@@ -1,12 +1,11 @@
 """
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
 from seaborn.categorical import _CategoricalPlotter
 import seaborn as sns
 
-from seaborn.utils import remove_na, _default_color
+from seaborn.utils import _default_color
 import warnings
 
 
@@ -14,29 +13,41 @@ import warnings
 class _MyCategoricalPlotter(_CategoricalPlotter):
     def __init__(self, width, **kwargs):
         super().__init__(**kwargs)
-        self._width = width 
+        self._width = width
 
     @property
     def _native_width(self):
         return self._width / 0.8
 
 
-
-# patched version to allow for hue offset 
+# patched version to allow for hue offset
 def mystripplot(
-    width=.1, 
-    data=None, *, x=None, y=None, hue=None, order=None, hue_order=None,
-    dodge=False, orient=None, color=None, palette=None,
-    size=5, 
-    edgecolor=None, 
+    width=0.1,
+    data=None,
+    *,
+    x=None,
+    y=None,
+    hue=None,
+    order=None,
+    hue_order=None,
+    dodge=False,
+    orient=None,
+    color=None,
+    palette=None,
+    size=5,
+    edgecolor=None,
     linewidth=0,
-    hue_norm=None, log_scale=None, native_scale=False, formatter=None, legend="auto",
+    hue_norm=None,
+    log_scale=None,
+    native_scale=False,
+    formatter=None,
+    legend="auto",
     ax=None,
     **kwargs
 ):
 
     p = _MyCategoricalPlotter(
-        width=width, 
+        width=width,
         data=data,
         variables=dict(x=x, y=y, hue=hue),
         order=order,
@@ -62,21 +73,21 @@ def mystripplot(
 
     p.map_hue(palette=palette, order=hue_order, norm=hue_norm)
     color = _default_color(ax.scatter, hue, color, kwargs)
-    
+
     if edgecolor is None:
         edgecolor = p._complement_color(edgecolor, color, p._hue_map)
-    
+
     kwargs.setdefault("zorder", 3)
     size = kwargs.get("s", size)
 
     kwargs.update(
-        s=size ** 2,
+        s=size**2,
         edgecolor=edgecolor,
         linewidth=linewidth,
     )
 
     p.plot_strips(
-        jitter=.5*width,
+        jitter=0.5 * width,
         dodge=dodge,
         color=color,
         plot_kws=kwargs,
@@ -89,36 +100,48 @@ def mystripplot(
     p._adjust_cat_axis(ax, axis=p.orient)
 
 
-
-def boxstripplot(x=None, y=None, data=None,
-                    width=0.2, size=2, 
-                    box_alpha = 0.9,  
-                    strip_alpha = 0.5,
-                    linewidth=0.5, 
-                    fliersize=0,
-                    showmeans=False,
-                     hue=None, order=None, hue_order=None, orient=None, color=None, palette=None,
-                     ax=None,
-                     box_kwargs=None, strip_kwargs=None, violin=False, saturation=None):
+def boxstripplot(
+    x=None,
+    y=None,
+    data=None,
+    width=0.2,
+    size=2,
+    box_alpha=0.9,
+    strip_alpha=0.5,
+    linewidth=0.5,
+    fliersize=0,
+    showmeans=False,
+    hue=None,
+    order=None,
+    hue_order=None,
+    orient=None,
+    color=None,
+    palette=None,
+    ax=None,
+    box_kwargs=None,
+    strip_kwargs=None,
+    violin=False,
+    saturation=None,
+):
     """
     plots a half-box plot with a regular boxplot and a stripplot next to each other
-      
-    parameters are like seaborn.boxplot 
+
+    parameters are like seaborn.boxplot
 
     Example:
     ========
-    
+
         import numpy as np
         import pandas as pd
-        
+
         x = np.random.uniform(0, 1, 100)
         y = np.random.uniform(.5, 1.2, 100)
-        
+
         df =  pd.DataFrame(dict(x=x, y=y))
 
         halfboxstripplot(data=df, width=.5,
                          strip_kwargs=dict(alpha=0.5))
-                         
+
     """
     if ax is None:
         ax = plt.gca()
@@ -126,41 +149,91 @@ def boxstripplot(x=None, y=None, data=None,
     if palette is None:
         palette = sns.color_palette("tab10")
 
-    if box_kwargs is None: 
-        box_kwargs = {} 
-        
-    if strip_kwargs is None: 
-        strip_kwargs = {} 
-        
-    box_kwargs.setdefault('boxprops', dict())
-    box_kwargs.setdefault('whiskerprops', dict())
-    box_kwargs.setdefault('capprops', dict())
-    box_kwargs.setdefault('meanprops', {"marker":"o",
-                                         "markerfacecolor":"none", 
-                                         "markeredgecolor":"black",
-                                         "markersize":"5"})
-    box_kwargs.setdefault('medianprops', {
-        "color": "black",
-        "alpha": 0.35,
-    }
+    if box_kwargs is None:
+        box_kwargs = {}
+
+    if strip_kwargs is None:
+        strip_kwargs = {}
+
+    box_kwargs.setdefault("boxprops", dict())
+    box_kwargs.setdefault("whiskerprops", dict())
+    box_kwargs.setdefault("capprops", dict())
+    box_kwargs.setdefault(
+        "meanprops",
+        {
+            "marker": "o",
+            "markerfacecolor": "none",
+            "markeredgecolor": "black",
+            "markersize": "5",
+        },
+    )
+    box_kwargs.setdefault(
+        "medianprops",
+        {
+            "color": "black",
+            "alpha": 0.35,
+        },
     )
 
-    box_kwargs['boxprops'].setdefault('alpha', box_alpha)
-    box_kwargs['whiskerprops'].setdefault('alpha', box_alpha)
-    box_kwargs['capprops'].setdefault('alpha', box_alpha)
+    box_kwargs["boxprops"].setdefault("alpha", box_alpha)
+    box_kwargs["whiskerprops"].setdefault("alpha", box_alpha)
+    box_kwargs["capprops"].setdefault("alpha", box_alpha)
     if saturation is not None:
-        box_kwargs['saturation'] = saturation
+        box_kwargs["saturation"] = saturation
 
-    strip_kwargs.setdefault('alpha', strip_alpha)
-
+    strip_kwargs.setdefault("alpha", strip_alpha)
 
     with warnings.catch_warnings():
-        warnings.filterwarnings('ignore')
+        warnings.filterwarnings("ignore")
         if not violin:
-            ax = sns.boxplot(data=data, x=x, y=y, width=width, hue=hue, order=order, showmeans=showmeans, hue_order=hue_order, orient=orient, color=color, palette=palette, fliersize=fliersize, ax=ax, **box_kwargs)
+            ax = sns.boxplot(
+                data=data,
+                x=x,
+                y=y,
+                width=width,
+                hue=hue,
+                order=order,
+                showmeans=showmeans,
+                hue_order=hue_order,
+                orient=orient,
+                color=color,
+                palette=palette,
+                fliersize=fliersize,
+                ax=ax,
+                **box_kwargs
+            )
         else:
-            ax = sns.violinplot(data=data, x=x, y=y, width=width, hue=hue, order=order, showmeans=showmeans, hue_order=hue_order, orient=orient, color=color, palette=palette, fliersize=fliersize, ax=ax, **box_kwargs)
-        ax = mystripplot(data=data, x=x, y=y, width=width, size=size,
-                         hue=hue, dodge=False if hue is None else True, 
-                         order=order, hue_order=hue_order, orient=orient, color=color, palette=palette, linewidth=linewidth, ax=ax, **strip_kwargs)
+            ax = sns.violinplot(
+                data=data,
+                x=x,
+                y=y,
+                width=width,
+                hue=hue,
+                order=order,
+                showmeans=showmeans,
+                hue_order=hue_order,
+                orient=orient,
+                color=color,
+                palette=palette,
+                fliersize=fliersize,
+                ax=ax,
+                **box_kwargs
+            )
+        ax = mystripplot(
+            data=data,
+            x=x,
+            y=y,
+            width=width,
+            size=size,
+            hue=hue,
+            dodge=False if hue is None else True,
+            order=order,
+            hue_order=hue_order,
+            orient=orient,
+            color=color,
+            palette=palette,
+            linewidth=linewidth,
+            ax=ax,
+            **strip_kwargs
+        )
     return ax
